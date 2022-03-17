7c8501f6d6ad8e121bb006ca8386541a25053c3b
Pierre Saulnier <saulnier254@gmail.com>
2022-03-17 11:53:17 +0100

    fixes + norm + cases double & single quote & trim case

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: psaulnie <psaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 16:04:55 by psaulnie          #+#    #+#             */
-/*   Updated: 2022/03/17 09:39:52 by psaulnie         ###   ########.fr       */
+/*   Updated: 2022/03/17 11:51:06 by psaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,18 @@ typedef struct s_index
 {
 	int	i;
 	int	j;
+	int	s_quote;
+	int	d_quote;
 }	t_index;
 
 /*	PARSING.C	*/
 
 t_list_char	*parsing(char *cmd);
+
+/*	PARSING_UTILS.C	*/
+
+int			skip_whitespace(char *cmd, int i);
+int			is_useless(char *command);
 
 /*	STR_UTILS.C	*/
 
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: psaulnie <psaulnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 17:04:02 by psaulnie          #+#    #+#             */
-/*   Updated: 2022/03/17 09:45:10 by psaulnie         ###   ########.fr       */
+/*   Updated: 2022/03/17 11:52:17 by psaulnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,6 @@
 	-minishell > echo "'hi'" > d | xargs;
 	- [echo "'hi'"] [>] [d] [|] [xargs]
 */
-
-static int	skip_whitespace(char *cmd, int i)
-{
-	while (cmd[i] == ' ' || cmd[i] == '\t'
-		|| cmd[i] == '\n' || cmd[i] == '\v'
-		|| cmd[i] == '\f' || cmd[i] == '\r')
-		i++;
-	return (i);
-}
-
-static int	is_useless(char *command)
-{
-	int	i;
-	int	is_useless;
-
-	i = 0;
-	is_useless = 1;
-	while (command[i])
-	{
-		if (!(command[i] == ' ' || command[i] == '\t' || command[i] == '\n'
-				|| command[i] == '\n' || command[i] == '\v'
-				|| command[i] == '\f' || command[i] == '\r'))
-			is_useless = 0;
-		i++;
-	}
-	return (is_useless);
-}
 
 static t_index	add_to_list(char *cmd, t_index index, t_list_char **list)
 {
@@ -88,29 +61,56 @@ static t_index	parsing_special_char(char *cmd, t_index index,
 	return (index);
 }
 
+static t_index	parsing_loop(char *cmd, t_index index, t_list_char **list)
+{
+	index.j = index.i;
+	while (cmd[index.j])
+	{
+		if (cmd[index.j] == '\'')
+			index.s_quote ^= 1;
+		if (cmd[index.j] == '"')
+			index.d_quote ^= 1;
+		if ((cmd[index.j] == '|' || cmd[index.j] == '>'
+				|| cmd[index.j] == '<' || cmd[index.j] == ';')
+			&& (index.d_quote == 0 && index.s_quote == 0))
+		{
+			index = parsing_special_char(cmd, index, list);
+			break ;
+		}
+		index.j++;
+	}
+	return (index);
+}
+
+static t_index	parsing_end(char *cmd, t_index index, t_list_char **list)
+{
+	index.i = skip_whitespace(cmd, index.i);
+	index = add_to_list(cmd, index, list);
+	if ((cmd[index.i - 1] == '|' || cmd[index.i - 1] == '>'
+			|| cmd[index.i - 1] == '<' || cmd[index.i - 1] == ';')
+		&& !(cmd[index.i] == ' ' || cmd[index.i] == '\t'
+			|| cmd[index.i] == '\n' || cmd[index.i] == '\n'
+			|| cmd[index.i] == '\v' || cmd[index.i] == '\f'
+			|| cmd[index.i] == '\r'))
+		index.i--;
+	if (cmd[index.i + 1])
+		index.i++;
+	return (index);
+}
+
 t_list_char	*parsing(char *cmd)
 {
 	t_list_char	*list;
 	t_index		index;
 
 	index.i = 0;
+	index.s_quote = 0;
+	index.d_quote = 0;
+	list = NULL;
 	while (cmd[index.i])
 	{
-		index.j = index.i;
-		while (cmd[index.j])
-		{
-			if (cmd[index.j] == '|' || cmd[index.j] == '>'
-				|| cmd[index.j] == '<' || cmd[index.j] == ';')
-			{
-				index = parsing_special_char(cmd, index, &list);
-				break ;
-			}
-			index.j++;
-		}
-		index.i = skip_whitespace(cmd, index.i);
-		index = add_to_list(cmd, index, &list);
-		if (cmd[index.i + 1])
-			index.i++;
+		index = parsing_loop(cmd, index, &list);
+		index = parsing_end(cmd, index, &list);
 	}
 	return (list);
 }

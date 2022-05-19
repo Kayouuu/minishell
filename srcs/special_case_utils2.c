6673133bb2bcce949cb137970b96b9b990560d4f
Loup Battesti <lbattest@z2r3p2.42lyon.fr>
2022-05-19 11:31:35 +0200

    i've finish my part i think

--- srcs/special_case_utils2.c
+++ srcs/special_case_utils2.c
@@ -6,11 +6,22 @@
 /*   By: lbattest <lbattest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:44:20 by lbattest          #+#    #+#             */
-/*   Updated: 2022/05/18 17:22:09 by lbattest         ###   ########.fr       */
+/*   Updated: 2022/05/19 11:28:57 by lbattest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+static int	is_equal(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		if (str[i] == '=')
+			return (1);
+	return (0);
+}
 
 void	write_env(t_env *env)
 {
@@ -19,7 +30,22 @@ void	write_env(t_env *env)
 	start = env->addon_env;
 	while (env->addon_env)
 	{
-		printf("%s\n", env->addon_env->content);
+		if (is_equal(env->addon_env->content) == 1)
+			printf("%s\n", env->addon_env->content);
+		env->addon_env = env->addon_env->next;
+	}
+	env->addon_env = start;
+	return ;
+}
+
+static void	export_no_arg(t_env *env)
+{
+	t_list_char	*start;
+
+	start = env->addon_env;
+	while (env->addon_env)
+	{
+		printf("declare -x %s\n", env->addon_env->content);
 		env->addon_env = env->addon_env->next;
 	}
 	env->addon_env = start;
@@ -34,7 +60,10 @@ void	export(char **list, t_env *env)
 	(void)env;
 	i = 0;
 	if (!list[1])
+	{
+		export_no_arg(env);
 		return ;
+	}
 	while (list[1][i] && list[1][i] != '=')
 		i++;
 	var = ft_substr(list[1], 0, ++i);
@@ -42,7 +71,6 @@ void	export(char **list, t_env *env)
 		return ;
 	env_replace_line(&env, var, &list[1][i]);
 	free(var);
-	return ;
 }
 
 void	leave_this(char **list)
@@ -51,11 +79,9 @@ void	leave_this(char **list)
 	long int	nbr;
 
 	i = -1;
-	printf("%s\n", list[1]);
 	while (list[1][++i])
 	{
-		//il manque des test car - + est aumoins good
-		if (ft_isdigit(list[1][i]) == 0)
+		if (list[1][i] < '!' || list[1][i] > '?')
 		{
 			ft_putendl_fd("minishell: exit: numeric argument required", 2);
 			exit (255);
@@ -66,6 +92,5 @@ void	leave_this(char **list)
 	if (nbr <= 0 && nbr >= 255)
 		exit (nbr);
 	else
-		exit(-1);
-		//pas encore trouver
+		exit (nbr % 256);
 }
